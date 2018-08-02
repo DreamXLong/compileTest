9e272e2403090b13b20d6f583667438b97c35024
DreamXLong <575695960@qq.com>
2018-08-02 16:58:36 +0800

    测试

---
+++ testlibrary/src/main/java/com/example/testlibrary/Test.java
@@ -0,0 +1,11 @@
+package com.example.testlibrary;
+
+/**
+ * Created by Administrator on 2018/8/2.
+ */
+
+public class Test {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
